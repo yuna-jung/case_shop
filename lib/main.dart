b20a93d9c26e96f606a94d091376d0449c1bc663
yuubc <yuna8.ca@gmail.com>
2025-12-31 17:40:25 +0900

    mywork_added_to_firstpull

--- lib/main.dart
+++ lib/main.dart
@@ -22,9 +22,34 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.lightBlueAccent,
           brightness: Brightness.light,
-          ),
+        ),
       ),
       home: const Homepage(),
     );
   }
 }
+
+
+//import 'package:flutter/material.dart';
+// import 'pages/product_list_page.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'CaseShop',
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),
+//         useMaterial3: true,
+//       ),
+//       home: const ProductListPage(),
+//       debugShowCheckedModeBanner: false,
+//     );
+//   }
+// }
